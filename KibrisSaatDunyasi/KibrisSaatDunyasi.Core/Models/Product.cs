daf10fa727ce6e04237575104190fd23bf58111a
MELEK GÖK <16330622@emu.edu.tr>
2021-01-21 00:33:11 +0200

    product model and productrepository in memory built

---
+++ KibrisSaatDunyasi/KibrisSaatDunyasi.Core/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KibrisSaatDunyasi.Core.Models
+{
+  public  class Product
+    {
+        public string productid { get; set; }
+        public string productname { get; set; }
+        public string category { get; set; }
+        public string description { get; set; }
+        public string image { get; set; }
+        public decimal price { get; set; }
+        public string brand { get; set; }
+
+        public Product()
+        {
+            this.productid = Guid.NewGuid().ToString();
+        }
+    }
+}
